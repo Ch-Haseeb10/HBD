64f0795ee152f619bfc1050cdb4f989b557aaa06
Mian Haseeb Amir <haseebamir322@gmail.com>
2025-02-14 17:58:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    document.getElementById("message").innerText = " Some bonds don’t need a name, some feelings don’t need words. No matter what you call it, you’ll always have a place in my heart that no one else can take. And that’s something even time can’t change.  I’m truly sorry for anything that upset you. You mean everything to me. I can’t imagine my life without you. Happy Birthday, and I hope you can forgive me. ❤️ unblock kro  @aye3ha._  .. ! .. @enhalvs";
+  }
